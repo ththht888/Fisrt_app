abdfe92144ecf635256d6ee4a200d5693d09f6cb
ththht888 <o.morozova2016@gmail.com>
2024-12-26 02:11:48 +0500

    task_5

--- app.js
+++ app.js
@@ -3,18 +3,38 @@ const btn = document.getElementById("btn-change-text");
 const parentDiv = document.getElementById("main");
 const input = document.getElementById("input-text");
 const select = document.getElementById("select");
+const inputNum = document.getElementById("input-number");
 
 let textInput = "";
+let numInput = "";
 let cardCount = 0;
 btn.disabled = true;
 
+function buttonState() {
+  const TextInputValid = textInput.trim().length > 0;
+  const NumInputValid = numInput.length === 11;
+  const SelectValid = select.value !== "";
+  btn.disabled = !(TextInputValid && NumInputValid && SelectValid);
+}
+
 input.addEventListener("input", function (event) {
   textInput = event.target.value;
-  if (textInput.length === 0) {
-    btn.disabled = true;
-  } else {
-    btn.disabled = false;
+  buttonState();
+});
+
+inputNum.addEventListener("keydown", function (event) {
+  if (event.key < "0" || (event.key > "9" && event.key !== "Backspace")) {
+    event.preventDefault();
   }
+});
+
+inputNum.addEventListener("input", function (event) {
+  numInput = event.target.value;
+  buttonState();
+});
+
+select.addEventListener("change", function () {
+  buttonState();
 });
 
 btn.addEventListener("click", function () {
@@ -23,18 +43,29 @@ btn.addEventListener("click", function () {
   const card = document.createElement("div");
   card.className = "card";
 
-  card.textContent = textInput;
+  const nameDisplay = document.createElement("p");
+  nameDisplay.textContent = `Имя: ${textInput}`;
+  card.appendChild(nameDisplay);
 
-  const selectedColor = select.value;
-  switch (selectedColor) {
+  const numberDisplay = document.createElement("p");
+  numberDisplay.textContent = `Телефон: ${numInput}`;
+  card.appendChild(numberDisplay);
+
+  const selectedOption = select.options[select.selectedIndex];
+  const jobDisplay = document.createElement("p");
+  jobDisplay.textContent = `Должность: ${selectedOption.text}`;
+  card.appendChild(jobDisplay);
+
+  const selectedValue = select.value;
+  switch (selectedValue) {
     case "red":
-      card.className = "card-red";
+      card.classList.add("card-red");
       break;
     case "yellow":
-      card.className = "card-yellow";
+      card.classList.add("card-yellow");
       break;
     case "green":
-      card.className = "card-green";
+      card.classList.add("card-green");
       break;
   }
   parentDiv.appendChild(card);
